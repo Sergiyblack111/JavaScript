e21a0aa38ffbdcd2c45c69190391e475f2f0a727
Sergiyblack111 <sergiyblack111@gmail.com>
2022-06-18 14:55:59 +0300

    practice

--- Less_8/mainJS.js
+++ Less_8/mainJS.js
@@ -1,41 +1,51 @@
 
 //- Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або
-// document.getElementsByTagName :
-
+//document.getElementsByTagName :
 
 //-- отримує текст з параграфа з id "content"
 let contentById = document.getElementById('content');
 console.log(contentById);
 
-// -- отримує текст з блоку з id "rules"
+//-- отримує текст з блоку з id "rules"
 let rulesById = document.getElementById('rules');
 console.log(rulesById);
 
-// -- замініть текст параграфа з id 'content' на будь-який інший
+//-- замініть текст параграфа з id 'content' на будь-який інший
 contentById.innerHTML = '<b>TEST TEST TEST Lorem ipson beauty today</b>'
 
-// -- замініть текст параграфа з id 'rules' на будь-який інший
+//-- замініть текст параграфа з id 'rules' на будь-який інший
 rulesById.innerHTML = '<h1>Декілька правил виконання ДЗ в OKTEN SCHOOL</h1>'
 
-// -- змініть кожному елементу колір фону на червоний
-let childrenBackground = document.body.children;
-for (const el of childrenBackground) {
-    el.style.backgroundColor = 'red';
+//-- змініть кожному елементу колір фону на червоний
+//let childrenBackground = document.body.children;
+let childrenBackground = document.body.getElementsByTagName('li');
+for (const elem of childrenBackground) {
+  elem.style.backgroundColor = 'red';
 }
 
-// -- змініть кожному елементу колір тексту на синій
+//-- змініть кожному елементу колір тексту на синій
 let childrenTextColor = document.body.children;
-for (const ol of childrenTextColor) {
-    ol.style.color = 'blue';
+for (const txt of childrenTextColor) {
+  txt.style.color = 'blue';
 }
 
-// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+//-- отримати весь список класів елемента з id=rules і вивести їх в console.log
 let rulesId = document.getElementById('rules');
 console.log(rulesId.classList);
 
-// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+//-- поміняти колір тексту у всіх елементів fc_rules на червоний
 let classRules = document.getElementsByClassName('fc_rules');
 for (const el of classRules) {
-    el.style.color = 'red'
+   el.style.color = 'green'
 }
 
+//  1:08:00
+// function scraper(startElement) {
+//     console.log(startElement);
+//     let children = startElement.children;
+//     for (const child of children) {
+//         scraper(child);
+//     }
+// }
+//
+// scraper(document.body);
