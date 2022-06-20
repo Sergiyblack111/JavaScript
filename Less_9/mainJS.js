2656420f6acaedd9bb0d6c12caa2dbadbc5655ff
Sergiyblack111 <sergiyblack111@gmail.com>
2022-06-20 21:14:58 +0300

    practice

--- Less_9/mainJS.js
+++ Less_9/mainJS.js
@@ -1,44 +1,48 @@
 // Все робити за допомоги js.
 // - створити блок,
-// let div = document.createElement('div');
 // - додати йому класи wrap, collapse, alpha, beta
-// div.classList.add('wrap');
-// div.classList.add('colapse');
-// div.classList.add('alpha');
-// div.classList.add('beta');
 // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
-// div.style.background = 'silver';
-// div.style.color = 'red';
-// div.style.color = '36px';
 // - додати цей блок в body.
-// let h1 = document.createElement('h1');
-// h1.innerText = '1';
-
-// let p = document.createElement('p');
-// p.innerText = 'hello world';
-
-// div.appendChild(h1);
-// div.appendChild(p);
-// document.body.appendChild(div);
 // - клонувати його повністю, та додати клон в body.
-//document.body.appendChild(div.cloneNode(true));
+// let block = document.createElement('div');
+//
+// block.classList.add('wrap');
+// block.classList.add('collapse');
+// block.classList.add('alpha');
+// block.classList.add('beta');
+//
+// block.style.width = '400px';
+// block.style.height = '400px';
+// block.style.background = 'green';
+//
+// let p = document.createElement('p');
+//
+// p.style.fontSize = '50px';
+// p.innerText = 'hello world';
+//
+// let h1 = document.createElement('h1');
+// h1.innerText = '!!!!!';
+//
+// block.append(p, h1);
+// document.body.appendChild(block);
+//
+// document.body.appendChild(block.cloneNode(true));
 
-/*// - Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
-// Завдання робити через цикли.
-for (const course of coursesAndDurationArray) {
-    let divElement = document.createElement('div');
-    divElement.innerText = `${course.title} ${course.monthDuration}`;
-    document.body.appendChild(divElement);
-}*/
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+
+// for (const course of coursesAndDurationArray) {
+//     let h1 = document.createElement('h1');
+//     h1.innerText = `Назва курсу-${course.title}, термін навчання-${course.monthDuration} місяців`;
+//     document.body.appendChild(h1);
+// }
 
 // - Є масив
 // let coursesAndDurationArray = [
@@ -55,27 +59,29 @@ for (const course of coursesAndDurationArray) {
 // Завдання робити через цикли.
 // for (const course of coursesAndDurationArray) {
 //     let divElement = document.createElement('div');
+//     divElement.classList.add('item');
 //     divElement.style.width = '400px';
-//     divElement.style.height = '100px';
+//     divElement.style.height = '110px';
 //     divElement.style.border = 'solid';
 //     divElement.style.borderColor = 'black';
 //
 //     let h1 = document.createElement('h1');
+//     h1.classList.add('heading')
 //     h1.innerText = course.title;
 //     h1.style.background = 'blue';
 //     h1.style.color = 'white';
 //
 //     let p = document.createElement('p');
+//     p.classList.add('description');
 //     p.innerText = course.monthDuration;
 //     p.style.background = 'yellow';
 //     p.style.color = 'black';
 //     p.style.width = '100px';
 //     p.style.height = '30px';
 //
-//     divElement.appendChild(h1);
-//     divElement.appendChild(p);
+//     divElement.append(h1, p);
 //     document.body.appendChild(divElement);
-//}
+// }
 
 // 21:16
 // let wrap = document.createElement('div');
