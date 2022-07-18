db32582cce85243347e24a9ca3ca0ce1ad7e7c0f
Sergiyblack111 <sergiyblack111@gmail.com>
2022-07-18 17:55:28 +0300

    practice

--- Less_10/mainJS.js
+++ Less_10/mainJS.js
@@ -1,14 +1,18 @@
 
 // - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике
 // на кнопку исчезал элемент с id="text".
-// - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
 
 // 1 варінт ===========================================================
-// let click = document.getElementById('text');
+// let txt = document.getElementById('text');
 // let button = document.getElementById('button');
 // button.onclick = function () {
-//     click.style.display = 'none';
+//     txt.style.display = 'none';
 // };
+
+// - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// let button = document.getElementById('button').onclick = function () {
+//     document.getElementById('button').hidden = true;
+// }
 
 // 2 варіант ===========================================================
 // document.getElementById('button').onclick = function () {
@@ -21,7 +25,7 @@
 //     let age = document.getElementById('age').value;
 //     if (!age) {
 //         alert('Ви не ввели свого віку');
-//     } else if (+age < 18) {
+//     } else if (age < 18) {
 //         alert('Вам менше 18');
 //     } else {
 //         alert('Вам більше 18');
@@ -29,15 +33,12 @@
 // }
 
 // - Создайте меню, которое раскрывается/сворачивается при клике
-// let menuElement = document.querySelector('.menu');
 // let titleElement = document.querySelector('.title');
-// let divElement = document.querySelector('.containerOhh');
+// let ulList = document.querySelector('.ulList');
 //
 // titleElement.onclick = function () {
-//     menuElement.classList.toggle('hidden');
-//     divElement.classList.toggle('containerOhh');
+//     ulList.classList.toggle('ul_display_none');
 // }
-
 
 // - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
 //     Вывести список комментариев в документ, каждый в своем блоке.
@@ -46,32 +47,32 @@
 // let comments = [
 //     {name: 'Максим', commentary: 'Вчи English'},
 //     {name: 'Іван', commentary: 'Досить сидіти, вже пізно'},
-//     {name: 'Льоня', commentary: 'Зупка, стине, ходи додому'},
+//     {name: 'Льоня', commentary: 'Cуп, стине, ходи додому'},
 //     {name: 'Іра', commentary: 'Воно, тобі треба?'},
 // ];
-
+//
 // let divFather = document.createElement('div');
-// for (const item of comments) {
+//
+// for (const element of comments) {
 //     let div = document.createElement('div');
 //     let h3 = document.createElement('h3');
 //     let p = document.createElement('p');
 //     let btn = document.createElement('button');
 //     let hr = document.createElement('hr');
 //
-//     h3.innerText = item.name;
-//     p.innerText = item.commentary;
+//     h3.innerText = element.name;
+//     p.innerText = element.commentary;
 //     btn.innerText = 'згорни/розгорни';
 //
-//     btn.onclick = () => {
-//         p.classList.toggle('pHidden');
-        //btn.previousElementSibling.classList.add('pHidden'); - 2 варіант
-    // }
-
-//     div.appendChild(h3);
-//     div.appendChild(p);
-//     div.appendChild(btn);
+//     div.append(h3, p, btn);
 //     divFather.append(div, hr);
+//
+//     btn.onclick = function () {
+//         p.classList.toggle('pHidden');
+//         // //btn.previousElementSibling.classList.add('pHidden'); - 2 варіант
+//     }
 // }
+//
 // document.body.appendChild(divFather);
 
 // прев'ю   10:05
--- Less_11/mainJS.js
+++ Less_11/mainJS.js
@@ -1,42 +1,143 @@
 
 // -створити форму з інпутами для name та age.
 //  При відправці форми записати об'єкт в localstorage
-//let {name, age} = document.forms.userForm; //через деструктуризацію
-let name = document.forms.userForm.name;
-let age = document.forms.userForm.age;
-let btn = document.getElementById('btn');
-let key = 'key';
+// let {name, age} = document.forms.userForm; //через деструктуризацію
+// //let name = document.forms.userForm.name;
+// //let age = document.forms.userForm.age;
+// let btn = document.getElementById('btn');
+// let key = 'key';
+//
+// let save = (userName, userAge) => {
+//     let user = {
+//         name: userName,
+//         age: userAge
+//     };
+//     localStorage.setItem(key, JSON.stringify(user));
+// };
+//
+// btn.onclick = (e) => {
+//     e.preventDefault();
+//     save(name.value, age.value);
+// }
 
-let save = (userName, userAge) => {
-    let user = {
-        name: userName,
-        age: userAge
-    };
-    localStorage.setItem(key, JSON.stringify(user));
-};
+// 2 варіант
+// let user = document.forms.userForm;
+// user.onsubmit = function (e) {
+//     e.preventDefault();
+//     localStorage.setItem('userValue', JSON.stringify({
+//         name: e.target.nameInput.value,
+//         age: e.target.ageInput.value
+//     }));
+// }
 
-btn.onclick = () => {
-    save(name.value, age.value);
-}
+// -створити форму з інпутами для model,type та volume автівки.
+// при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+// let {model, type, volume} = document.forms.carForm; // деструктуризація
+// //let model = document.forms.carForm.model;
+// //let type = document.forms.carForm.type;
+// //let volume = document.forms.carForm.volume;
+// let btnCar = document.getElementById('btnCar');
+// let item = 'keyCar';
+//
+// let saveCar = (modelCarValue, typeCarValue, volumeCarValue) => {
+//     let ollInformCar = {
+//         modelValue: modelCarValue,
+//         typeValue: typeCarValue,
+//         volumeValue: volumeCarValue
+//     }
+//     localStorage.setItem(item, JSON.stringify(ollInformCar));
+// };
+//
+// btnCar.onclick = (e) => {
+//     e.preventDefault();
+//     saveCar(model.value, type.value, volume.value);
+// }
 
+// прев'ю 17:15
+// let users = [
+//     {name: 'sergii', age: 23,},
+//     {name: 'oliya', age: 24,},
+//     {name: 'stepan', age: 25},
+//     {name: 'evgen', age: 26},
+//     {name: 'alex', age: 27}
+// ];
+//
+// localStorage.setItem('users', JSON.stringify(users));
+// let usersJSON = localStorage.getItem('users');
+// console.log(usersJSON);
+// let parse = JSON.parse(usersJSON);
+// console.log(parse);
+// parse.push({});
+// console.log(parse);
+// localStorage.setItem('users', JSON.stringify(parse));
 
-// // -створити форму з інпутами для model,type та volume автівки.
-// //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
-//let {model, type, volume} = document.forms.carForm; // деструктуризація
-let model = document.forms.carForm.model;
-let type = document.forms.carForm.type;
-let volume = document.forms.carForm.volume;
-let btnCar = document.getElementById('btnCar');
-let item = 'keyCar';
+// прев'ю 26:24
+// document.cookie = 'name = kokos';
+// document.cookie = 'surname = kokosov';
+// document.cookie = 'token = qwe;max-age=2344;path=/';
+//
+// let cookies = document.cookie.split(/;/);
+//
+// console.log(cookies);
+//
+// let obj = {};
+//
+// for (let i = 0; i < cookies.length; i++) {
+//     const cookie = cookies[i].split(/=/);
+//     obj[cookie[0]] = cookie[1];
+// }
+//
+// console.log(obj);
 
-let saveCar = (modelCar, typeCar, volumeCar) => {
-    let newArr = JSON.parse(localStorage.getItem(item));
+// 45:57
+// let users = [
+//     {name: 'sergii', age: 28, country: 'ukraine', status: true},
+//     {name: 'oliya', age: 28, country: 'ukraine', status: false},
+//     {name: 'stepan', age: 28, country: 'ukraine', status: true},
+//     {name: 'evgen', age: 28, country: 'ukraine', status: false},
+//     {name: 'alex', age: 28, country: 'ukraine', status: true},
+// ];
+//
+// for (const user of users) {
+//     let divUser = document.createElement('div');
+//     divUser.innerText = user.name;
+//     let addToCartButton = document.createElement('button');
+//     addToCartButton.innerText = 'add to cart';
+//     divUser.appendChild(addToCartButton);
+//     addToCartButton.onclick = function () {
+//         let users = localStorage.getItem('users'); // json string
+//         console.log(users);
+//         if (!users) {
+//             users = [];
+//             users.push(user);
+//             let usersArrJson = JSON.stringify(users);
+//             localStorage.setItem('users', usersArrJson);
+//         } else {
+//             let realArrayOfUsersObject = JSON.parse(users);
+//             realArrayOfUsersObject.push(user);
+//             localStorage.setItem('users', JSON.stringify(realArrayOfUsersObject));
+//         }
+//     }
+//     document.body.appendChild(divUser);
+// }
 
-    newArr.push({modelCar, typeCar, volumeCar})
-    localStorage.setItem(item, JSON.stringify(newArr));
-};
+// 58:40
+// document.cookie = 'name = kokos';
+// document.cookie = 'surname = abrikosov';
+// document.cookie = 'age = 31';
+// document.cookie = 'status = true';
+// console.log(document.cookie);
+//
+// let cookies = document.cookie.split(';');
+// console.log(cookies);
 
-btnCar.onclick = () => {
-    saveCar(model.value, type.value, volume.value);
-}
+// //let firstCookie = cookies[0];
+// //console.log(firstCookie);
 
+// let obj = {};
+// for (let i = 0; i < cookies.length; i++) {
+//     const cookie = cookies[i].split('='); //[name, kokos] [surname, abrikosov]
+//     obj[cookie[0]] = cookie[i];
+// }
+//
+// console.log(obj);
